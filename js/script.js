da5643da72d6969e147a4b2636ee58da40914275
Sergey <example@gmail.com>
2020-09-21 23:16:04 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+//alert('Hello');
+
+//const result = confirm("Are you here?");
+
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(answer + 4);
+
+const answers =[];
+
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Фамилия?', '');
+answers[2] = prompt('Сколько вам лет?', '');
+
+document.write(answers);
